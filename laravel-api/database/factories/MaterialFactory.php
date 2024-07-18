e6d0d072a62fe99a6c4b7c6c190582c0a867c2fd
Diego Forero <diego.forero@scrd.gov.co>
2024-07-17 22:09:37 -0500

    seeders y factories

---
+++ laravel-api/database/factories/MaterialFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Material>
+ */
+class MaterialFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'codigo' => $this->faker->unique()->word,
+            'descripcion' => $this->faker->sentence,
+            'unidad' => $this->faker->randomElement(['m2', 'und', 'kg']),
+            'precio' => $this->faker->randomFloat(2, 10, 1000),
+        ];
+    }
+}
---
+++ laravel-api/database/seeders/MaterialsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Material;
+
+class MaterialsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Material::factory()->count(50)->create();
+    }
+}
