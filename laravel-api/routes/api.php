ddfd2d9ea7ca45ef7d39cc9edd68fbd288071503
Diego Forero <diego.forero@scrd.gov.co>
2024-07-19 18:41:35 -0500

    nuevo endpoint parametro-detalle

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MaterialController;
 use App\Http\Controllers\ProyectoController;
 use App\Http\Controllers\ProyectoMaterialController;
+use App\Http\Controllers\ParametroDetalleController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,5 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('/materials',MaterialController::class);
 Route::apiResource('/proyects',ProyectoController::class);
 Route::apiResource('/proyects-materials',ProyectoMaterialController::class);
+
+Route::get('/parametro-detalle/{id}', [ParametroDetalleController::class, 'getByParametro']);
